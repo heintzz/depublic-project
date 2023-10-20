9a744a5d6023e6c6035796ccd7cf1f1d6e91e877
heintzz <mhasnanr@gmail.com>
2023-10-20 20:33:48 +0700

    update: highlight card slider

--- src/components/Home/HighlightCarousel.tsx
+++ src/components/Home/HighlightCarousel.tsx
@@ -1,29 +1,63 @@
 import { Pagination } from "swiper/modules";
-
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
 import "swiper/css/pagination";
+import "./HighlightCarousel.css";
+
+import Btob from "../../assets/images/btob.png";
+import Maher from "../../assets/images/maher-zain.png";
+
+const highlight = [
+  {
+    name: "BTOB",
+    image: Btob,
+  },
+  {
+    name: "Maher Zain",
+    image: Maher,
+  },
+  {
+    name: "BTOB",
+    image: Btob,
+  },
+  {
+    name: "Maher Zain",
+    image: Maher,
+  },
+  {
+    name: "BTOB",
+    image: Btob,
+  },
+];
 
 export default function HighlightCarousel() {
   return (
     <>
       <Swiper
         modules={[Pagination]}
-        pagination={{ clickable: true }}
-        slidesPerView={3}
         loop={true}
-        spaceBetween={7}
-        centeredSlides={true}
+        pagination={{ clickable: true, dynamicMainBullets: 2 }}
+        slidesPerView="auto"
         initialSlide={3}
+        centeredSlides={true}
+        spaceBetween={10}
         autoplay={{
           delay: 1000,
-          disableOnInteraction: false,
         }}
       >
-        {Array.from({ length: 7 }).map((_, index) => (
-          <SwiperSlide key={index}>
-            <div className="bg-red-200 h-[200px] rounded-2xl"></div>
+        {highlight.map((item, index) => (
+          <SwiperSlide key={index} className=" relative  h-[200px] max-w-[calc(100%-54px)] mb-12">
+            {({ isActive }) => (
+              <div
+                style={{
+                  backgroundImage: `url(${item.image})`,
+                }}
+                className={` ${
+                  isActive ? "h-[200px]" : "h-[180px] mt-[10px]"
+                } rounded-2xl bg-cover`}
+              ></div>
+            )}
           </SwiperSlide>
         ))}
       </Swiper>
